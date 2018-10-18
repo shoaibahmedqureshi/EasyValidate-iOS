5bf38ec0c3e9dbcf33e55d9bd1477b3a45da9f74
Shoaib Ahmed Qureshi <08bitsaqureshi@seecs.edu.pk>
2018-10-19 04:38:53 +0500

    Improved email validation regex
    Improved email validation regex to make it more accurate while validating.

--- EasyValidate-iOS/EmailChecker.m
+++ EasyValidate-iOS/EmailChecker.m
@@ -20,7 +20,7 @@
 @implementation EmailChecker
 -(bool) validate:(NSString*) value{
     return [StringCheckerUtil isMatch:value
-                    regex:@"^([a-zA-Z0-9_\\.\\-])+\\@(([a-zA-Z0-9\\-])+\\.)+([a-zA-Z0-9]{2,4})+$"];
+                    regex:@"^[_A-Za-z0-9-+]+(\\.[_A-Za-z0-9-+]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z‌​]{2,})$"];
 }
 
 -(NSString *) getErrorMessage{
